cffc37236059881a1366e7c0b6d812d823052c8a
Bruno César <bcgmeireles@gmail.com>
2023-07-09 18:31:07 -0300

    Organizing design of the drawer

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import Movies from '../pages/Movies';
-import StackRoutes from './StackRoutes';
+import StackRoutes from './stackRoutes';
 
 const Drawer = createDrawerNavigator();
 
 function Routes(){
     return(
-        <Drawer.Navigator>
+        <Drawer.Navigator screenOptions={{
+            headerShown: false,
+            drawerStyle: {
+                backgroundColor: '#090A0E',
+                paddingTop: 20,
+            },
+
+            drawerActiveBackgroundColor: '#E72F49',
+            drawerActiveTintColor: '#FFF',
+
+            drawerInactiveTintColor: '#FFF',
+            }}>
             <Drawer.Screen name='HomeDrawer' component={StackRoutes} />
             <Drawer.Screen name='Movies' component={Movies} />
         </Drawer.Navigator>
