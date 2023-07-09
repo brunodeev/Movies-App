a471f65b36d9f07251d3fca5860280a4705aeb33
Bruno César <bcgmeireles@gmail.com>
2023-07-09 17:21:47 -0300

    Implementing stack and drawer navigation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import Home from '../pages/Home';
 import Movies from '../pages/Movies';
+import StackRoutes from './StackRoutes';
 
 const Drawer = createDrawerNavigator();
 
 function Routes(){
     return(
         <Drawer.Navigator>
-            <Drawer.Screen name='Home' component={Home} />
+            <Drawer.Screen name='HomeDrawer' component={StackRoutes} />
             <Drawer.Screen name='Movies' component={Movies} />
         </Drawer.Navigator>
     )
