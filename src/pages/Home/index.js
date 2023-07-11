002ef8fec8b9d711b53604e49ead48ce370f4bd6
Bruno César <bcgmeireles@gmail.com>
2023-07-10 22:39:49 -0300

    Finishing the top screen layout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-import { Container } from './styles';
+import { Container, SearchContainer, Input, SearchButton } from './styles';
+
 import Header from '../../components/Header';
+import { Feather } from '@expo/vector-icons';
+
 
 function Home(){
     return(
         <Container>
             <Header title={'React Prime'}/>
-            <Text>Tela Home</Text>
+            <SearchContainer>
+                <Input
+                    placeholder='Ex.: Vingadores'
+                />
+                <SearchButton>
+                    <Feather name='search' size={28} color='#FFF' />
+                </SearchButton>
+            </SearchContainer>
         </Container>
     )
 }
