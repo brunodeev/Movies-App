d8b5a56a11fab7958831382f1bcc9d0ce1f5db96
Bruno César <bcgmeireles@gmail.com>
2023-07-18 18:19:43 -0300

    Adding api way in the home index

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScrollView } from 'react-native';
 
 import { 
@@ -12,16 +12,60 @@ import {
 
 import Header from '../../components/Header';
 import { Feather } from '@expo/vector-icons';
-import SliderItem from '../../components/SliderItem'
+import SliderItem from '../../components/SliderItem';
+
+import api, { key } from '../../services/api';
 
 
 function Home(){
+
+    const [nowMovies, setNowMovies] = useState([]);
+    const [popularMovies, setPopularMovies] = useState([]);
+    const [topMovies, setTopMovies] = useState([]);
+
+    useEffect(() => {
+
+        let isActive = true;
+
+        async function getMovies(){
+            const [nowData, popularData, topData] = await Promise.all([
+                api.get('/movie/now_playing', {
+                    params: {
+                        api_key: key,
+                        language: 'pt-BR',
+                        page: 1
+                    }
+                }),
+                api.get('/movie/popular', {
+                    params: {
+                        api_key: key,
+                        language: 'pt-BR',
+                        page: 1
+                    }
+                }),
+                api.get('/movie/top_rated', {
+                    params: {
+                        api_key: key,
+                        language: 'pt-BR',
+                        page: 1
+                    }
+                })
+            ]);
+            console.log(response.data);
+
+        };
+
+        getMovies();
+
+    }, []);
+
     return(
         <Container>
             <Header title={'React Prime'}/>
             <SearchContainer>
                 <Input
                     placeholder='Ex.: Vingadores'
+                    placeholderTextColor='#ddd'
                 />
                 <SearchButton>
                     <Feather name='search' size={28} color='#FFF' />
